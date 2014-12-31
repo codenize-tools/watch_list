043a78d77aae2a5353852d7570d55cd687bd91e8
Genki Sugawara <sugawara@cookpad.com>
2014-12-31 15:27:23 +0900

    Fix message

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,14 @@
 require 'watch_list'
 
+TEST_WATCH_LIST_API_KEY = ENV['TEST_WATCH_LIST_API_KEY']
+
 RSpec.configure do |config|
   config.before(:each) do
-    # XXX:
+    cleanup_uptimerobot
   end
+end
+
+def cleanup_uptimerobot
+  client = UptimeRobot::Client.new(apiKey: TEST_WATCH_LIST_API_KEY)
+  p client.getMonitors
 end
