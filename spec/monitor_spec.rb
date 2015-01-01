7bd15274c0aaaadfb91b8d33a6fba1523fe915a2
Genki Sugawara <sugawara@cookpad.com>
2015-01-01 10:51:00 +0900

    Fix monitor spec

--- spec/monitor_spec.rb
+++ spec/monitor_spec.rb
@@ -175,5 +175,181 @@ describe 'monitor' do
            :Interval=>5}},
        :alert_contacts=>[{:Type=>2, :Value=>WATCH_LIST_TEST_EMAIL}]}
     )
+
+    watch_list do
+      <<-RUBY
+        monitor "http monitor" do
+          target "http://example.com/test"
+          interval 6
+          paused false
+          alert_contact :email, "#{WATCH_LIST_TEST_EMAIL}"
+          type :http
+        end
+
+        monitor "http monitor2 (basic auth)" do
+          target "http://example.com/test"
+          interval 7
+          paused false
+          alert_contact :email, "#{WATCH_LIST_TEST_EMAIL}"
+
+          type :http do
+            httpusername "username2"
+            httppassword "password2"
+          end
+        end
+
+        monitor "keyword monitor" do
+          target "http://example.com/test"
+          interval 8
+          paused false
+          alert_contact :email, "#{WATCH_LIST_TEST_EMAIL}"
+
+          type :keyword do
+            keywordtype :not_exists
+            keywordvalue "Example Domain2"
+          end
+        end
+
+        monitor "keyword monitor (basic auth)" do
+          target "http://example.com/test"
+          interval 9
+          paused false
+          alert_contact :email, "#{WATCH_LIST_TEST_EMAIL}"
+
+          type :keyword do
+            keywordtype :not_exists
+            keywordvalue "(Example Domain2)"
+            httpusername "(username2)"
+            httppassword "(password2)"
+          end
+        end
+
+        monitor "ping monitor" do
+          target "127.0.0.2"
+          interval 10
+          paused false
+          alert_contact :email, "#{WATCH_LIST_TEST_EMAIL}"
+          type :ping
+        end
+
+        monitor "port monitor" do
+          target "google.com"
+          interval 11
+          paused false
+          alert_contact :email, "#{WATCH_LIST_TEST_EMAIL}"
+
+          type :port do
+            subtype :https
+            port 443
+          end
+        end
+
+        monitor "port monitor (custom)" do
+          target "google.com"
+          interval 12
+          paused false
+          alert_contact :email, "#{WATCH_LIST_TEST_EMAIL}"
+
+          type :port do
+            subtype :custom
+            port 18080
+          end
+        end
+
+        alert_contact do
+          type :email
+          value "#{WATCH_LIST_TEST_EMAIL}"
+        end
+      RUBY
+    end
+
+    expect(watch_list_export {|h| h[:monitors]['port monitor (custom)'][:URL] != 'example.com' }).to eq(
+      {:monitors=>
+        {"http monitor"=>
+          {:FriendlyName=>"http monitor",
+           :URL=>"http://example.com/test",
+           :Type=>1,
+           :SubType=>0,
+           :Port=>0,
+           :KeywordType=>nil,
+           :KeywordValue=>nil,
+           :HTTPUsername=>nil,
+           :HTTPPassword=>nil,
+           :AlertContacts=>[{:Type=>2, :Value=>WATCH_LIST_TEST_EMAIL}],
+           :Interval=>6},
+         "keyword monitor"=>
+          {:FriendlyName=>"keyword monitor",
+           :URL=>"http://example.com/test",
+           :Type=>2,
+           :SubType=>0,
+           :Port=>0,
+           :KeywordType=>2,
+           :KeywordValue=>"Example Domain2",
+           :HTTPUsername=>nil,
+           :HTTPPassword=>nil,
+           :AlertContacts=>[{:Type=>2, :Value=>WATCH_LIST_TEST_EMAIL}],
+           :Interval=>8},
+         "keyword monitor (basic auth)"=>
+          {:FriendlyName=>"keyword monitor (basic auth)",
+           :URL=>"http://example.com/test",
+           :Type=>2,
+           :SubType=>0,
+           :Port=>0,
+           :KeywordType=>2,
+           :KeywordValue=>"(Example Domain2)",
+           :HTTPUsername=>"(username2)",
+           :HTTPPassword=>"(password2)",
+           :AlertContacts=>[{:Type=>2, :Value=>WATCH_LIST_TEST_EMAIL}],
+           :Interval=>9},
+         "ping monitor"=>
+          {:FriendlyName=>"ping monitor",
+           :URL=>"127.0.0.2",
+           :Type=>3,
+           :SubType=>0,
+           :Port=>0,
+           :KeywordType=>nil,
+           :KeywordValue=>nil,
+           :HTTPUsername=>nil,
+           :HTTPPassword=>nil,
+           :AlertContacts=>[{:Type=>2, :Value=>WATCH_LIST_TEST_EMAIL}],
+           :Interval=>10},
+         "port monitor"=>
+          {:FriendlyName=>"port monitor",
+           :URL=>"google.com",
+           :Type=>4,
+           :SubType=>2,
+           :Port=>443,
+           :KeywordType=>nil,
+           :KeywordValue=>nil,
+           :HTTPUsername=>nil,
+           :HTTPPassword=>nil,
+           :AlertContacts=>[{:Type=>2, :Value=>WATCH_LIST_TEST_EMAIL}],
+           :Interval=>11},
+         "port monitor (custom)"=>
+          {:FriendlyName=>"port monitor (custom)",
+           :URL=>"google.com",
+           :Type=>4,
+           :SubType=>99,
+           :Port=>18080,
+           :KeywordType=>nil,
+           :KeywordValue=>nil,
+           :HTTPUsername=>nil,
+           :HTTPPassword=>nil,
+           :AlertContacts=>[{:Type=>2, :Value=>WATCH_LIST_TEST_EMAIL}],
+           :Interval=>12},
+         "http monitor2 (basic auth)"=>
+          {:FriendlyName=>"http monitor2 (basic auth)",
+           :URL=>"http://example.com/test",
+           :Type=>1,
+           :SubType=>0,
+           :Port=>0,
+           :KeywordType=>nil,
+           :KeywordValue=>nil,
+           :HTTPUsername=>"username2",
+           :HTTPPassword=>"password2",
+           :AlertContacts=>[{:Type=>2, :Value=>WATCH_LIST_TEST_EMAIL}],
+           :Interval=>7}},
+       :alert_contacts=>[{:Type=>2, :Value=>WATCH_LIST_TEST_EMAIL}]}
+    )
   end
 end
